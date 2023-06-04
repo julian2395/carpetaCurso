d7f601609ade5602a1f6c1a32c8daa557683de76
julian2395 <135543973+julian2395@users.noreply.github.com>
2023-06-04 20:11:00 +0000

    PROYECTO 1

---
+++ proyecto.py
@@ -0,0 +1,30 @@
+# MOstraremos mensaje de bienvenida
+
+print("Binvenido")
+ #pediremos al usuario intruducir su nombre
+n= input("Intruduce nombre por favor: " )   
+#pediremos al usuario inteducir su apellido paterno
+
+n = input("introduce apellido paterno por favor: " ) 
+
+#pediremos al usuario inteducir su apellido materno
+n = input("inteduce apellido materno: " )
+
+# pediremos al usuario inteducir su edad 
+e = int(input("su edad en años por favor: "))
+#pediremos al usuario su altura en metros
+
+a = float(input("intrudicir su altura en metro por favor: "))
+ 
+
+#pediremos su masa en kilogramos
+m = float(input("su masa en kilogramos por favor: "))
+
+
+IMC = m / a ** 2
+print("IMC " + str(IMC))
+
+
+
+
+
